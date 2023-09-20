ee9140571ef652502b9d3dad02bdb36b6b6b3fb8
Param934 <108082874+Param934@users.noreply.github.com>
2023-09-21 01:29:20 +0530

    Update sched_OTHER.c

--- Process Scheduling/sched_OTHER.c
+++ Process Scheduling/sched_OTHER.c
@@ -7,7 +7,7 @@ int main() {
     param.sched_priority = 0; // Default Priority for SCHED_OTHER
      if (sched_setscheduler(0, SCHED_OTHER, &param) == -1) {
             perror("sched_setscheduler");
-            exit("EXIT_FAILURE");
+            exit(EXIT_FAILURE);
         }
     uint64_t count = 0;
     for (uint64_t i = 1; i <= (1ULL << 32); i++) {
