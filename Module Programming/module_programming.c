28b0598f99a163b5161246ac7a4b69694f62148b
Param934 <108082874+Param934@users.noreply.github.com>
2023-09-21 02:07:44 +0530

    Update module_programming.c

--- Module Programming/module_programming.c
+++ Module Programming/module_programming.c
@@ -4,29 +4,30 @@
 #include <linux/cdev.h>
 #include <linux/proc_fs.h>
 #include <linux/slab.h>
+#include <linux/module.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Param Kumar Raman");
 MODULE_DESCRIPTION("Counting No. of running processes");
 
 void count_running_processes(void) {
-  int count = 0;
-  struct task_struct *task;
-  for_each_process(task) {
-    count++;
-  }
-  printk(KERN_INFO "Number of Running Processes = %d\n", count);
+    int count = 0;
+    struct task_struct *task;
+    for_each_process(task) {
+        if (task -> state == TASK_RUNNING) count++;
+    }
+    printk(KERN_INFO "Number of Running Processes = %d\n", count);
 }
 
 int proc_init(void) {
-  printk(KERN_INFO "Kernel Module Loaded\n");
-  count_running_processes();
-  return 0;
+    printk(KERN_INFO "Kernel Module Loaded\n");
+    count_running_processes();
+    return 0;
 }
 
 void proc_cleanup(void) {
   printk(KERN_INFO "Kernel Module Removed\n");
 }
 
-module_init(proc_init);_
+module_init(proc_init);
 module_exit(proc_cleanup);
