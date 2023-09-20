57f0bf117f7365ddbedd91139ed3c637f0efe8fc
Param934 <108082874+Param934@users.noreply.github.com>
2023-09-21 01:30:00 +0530

    Update sched_RR.c

--- Process Scheduling/sched_RR.c
+++ Process Scheduling/sched_RR.c
@@ -7,7 +7,7 @@ int main() {
     param.sched_priority = 1; // Default Priority for SCHED_RR
     if (sched_setscheduler(0, SCHED_RR, &param) == -1) {
         perror("sched_setscheduler");
-        exit("EXIT_FAILURE");
+        exit(EXIT_FAILURE);
     }
 
     uint64_t count = 0;
