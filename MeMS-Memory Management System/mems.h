28c31e99e84967ff8ad333d04f2b4788cd52a377
Param934 <108082874+Param934@users.noreply.github.com>
2023-11-01 15:26:33 +0530

    starting again

--- MeMS-Memory Management System/mems.h
+++ MeMS-Memory Management System/mems.h
@@ -13,6 +13,7 @@ REFER DOCUMENTATION FOR MORE DETAILS ON FUNSTIONS AND THEIR FUNCTIONALITY
 #include<stdio.h>
 #include<stdlib.h>
 #include <sys/mman.h>
+#include <math.h>
 
 /*
 Use this macro where ever you need PAGE_SIZE.
@@ -21,14 +22,19 @@ macro to make the output of all system same and conduct a fair evaluation.
 */
 #define PAGE_SIZE 4096
 
+struct header{
+	struct mainChainNode* mainChainNode;
+}
 // Define the main chain node structure
 struct mainChainNode{
 	void* mainAddr; // MeMS virtual address of main node
+	void* startingPhysicalAddr;
+	int pageCount=0;
 	struct subChainNode* subChain;
 	struct mainChainNode* prev;
 	struct mainChainNode* next;
+	size_t unallocatedMem=pageCount*PAGE_SIZE;
 }
-
 // Define the sub-chain node structure
 struct subChainNode{
 	void* subAddr;  // MeMS virtual address of the sub node
@@ -47,29 +53,10 @@ Input Parameter: Nothing
 Returns: Nothing
 */
 void mems_init(){
-	struct mainChainNode* mainChainHead = NULL;
-	void* memsHeapStart = NULL;	// Starting MeMS virtual address
-	void* addr = NULL;
-	int protection = PROT_READ | PROT_WRITE; //readable and writable
-	int map_flags = MAP_PRIVATE | MAP_ANONYMOUS;
-	int fd= -1;	// Passing an invalid file descriptor
-	off_t offset=0;
-	
-	//Initializing the main chain
-	mainChainHead= (struct mainChainNode*)mmap(NULL, sizeof(struct mainChainNode), protection, map_flags, fd, offset);
-    if (mainChainHead== MAP_FAILED) {
-        perror("failed to initialize mainChainHead");
-        exit(1);
-    }
-	memsHeapStart = mmap(addr, PAGE_SIZE, protection, map_flags, fd, offset);
-    if (memsHeapStart == MAP_FAILED) {
-        perror("failed to initialize memsHeapStart");
-        exit(1);
-    }
-    mainChainHead->mainAddr = memsHeapStart;
-    mainChainHead->subChain = NULL;
-    mainChainHead->prev = NULL;
-    mainChainHead->next = NULL;
+	//initializing Head
+	struct header* head = NULL;
+	int starting_v_addr=0;
+	int totalPageCount=0;
 }
 
 /*
@@ -79,45 +66,106 @@ Input Parameter: Nothing
 Returns: Nothing
 */
 void mems_finish(){
-    if (memsHeapStart) {
-        if (munmap(memsHeapStart, PAGE_SIZE)) == -1{
-			perror("error in unmapping memory of memsHeapStart");
-			exit(1);
-		}
-    }
+	}
 
-    // Iterate through the main chain and unmap both main and sub-chain nodes
-    struct mainChainNode* currentMain = mainChainhead;
-    while (currentMain) {
-        struct subChainNode* currentSub = currentMain->subChain;
-        while (currentSub) {
-            struct subChainNode* temp_sub = currentSub;
-            currentSub = currentSub->next;
-            munmap(temp_sub, sizeof(struct subChainNode));
-        }
-        struct mainChainNode* temp_main = currentMain;
-        currentMain = currentMain->next;
-        munmap(temp_main, sizeof(struct mainChainNode));
-    }
+
+void addSubChainNode(void* prevNode, size_t size){
+	struct subChainNode* subChainNode=(struct subChainNode*)mmap(NULL, sizeof(struct subChainNode), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if (subChainNode == MAP_FAILED) {
+		perror("mmap");
+		exit(1);
+	}
+	struct subChainNode* nextNode=prevNode->next;
+	subChainNode->subAddr=prevNode->subAddr+prevNode->segmentSize;
+	subChainNode->segmentSize=size;
+	subChainNode->next=nexNode;
+	prevNode->next=subChainNode;
+	// return subChainNode;
 }
 
+struct mainChainNode* addMainChainNode(void* prevNode, size_t size){
+	void* addr = NULL;
+	int protection = PROT_READ | PROT_WRITE; //readable and writable
+	int map_flags = MAP_PRIVATE | MAP_ANONYMOUS;
+	int fd= -1;	// Passing an invalid file descriptor
+	off_t offset=0;
+	void* memsHeapStart = mmap(addr, PAGE_SIZE*ceil(size/PAGE_SIZE), protection, map_flags, fd, offset);
+	if (memsHeapStart == MAP_FAILED) {
+		perror("failed to initialize memsHeapStart");
+		exit(1);
+	}
+	struct mainChainNode* mainChainNode=(struct mainChainNode*)mmap(NULL, sizeof(struct mainChainNode), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if (mainChainNode == MAP_FAILED) {
+		perror("mmap");
+		exit(1);
+	}
+	mainChainNode->mainAddr = starting_v_addr*totalPageCount;
+	mainChainNode->startingPhysicalAddr = memsHeapStart;
+	mainChainNode->pageCount=ceil(size/PAGE_SIZE);
+	mainChainHead->subChain = NULL;
+	mainChainHead->prev = NULL;
+	mainChainHead->next = NULL;
+	mainChainNode->unallocatedMem = mainChainNode->pageCount*PAGE_SIZE;
+	return mainChainNode;
+}
 
 /*
-Allocates memory of the specified size by reusing a segment from the free list if 
-a sufficiently large segment is available. 
+Allocates memory of the specified size by reusing a segment from the free list if
+a sufficiently large segment is available.
 
-Else, uses the mmap system call to allocate more memory on the heap and updates 
+Else, uses the mmap system call to allocate more memory on the heap and updates
 the free list accordingly.
 
 Note that while mapping using mmap do not forget to reuse the unused space from mapping
 by adding it to the free list.
 Parameter: The size of the memory the user program wants
 Returns: MeMS Virtual address (that is created by MeMS)
-*/ 
+*/
+
 void* mems_malloc(size_t size){
-
+	if (head->MainChainNode){
+		// Find a sufficiently large segment in the free list
+		struct mainChainNode* currentMain = head->mainChainNode;
+		while (currentMain) {
+			if (currentMain-> unallocatedMem >= size) {//No need to allocate new memory
+				struct subChainNode* currentSub = current->subChainNode;
+				while (currentSub){
+					if (currentSub->is_hole) {
+						if (currentSub->segmentSize=size){
+							currentSub->is_hole=0;
+							return currentSub->subAddr;
+						}else if (currentSub->segmentSize>size){
+							remainingSize=segmentSize-size;
+							currentSub->segmentSize=size;
+							currentSub->is_hole=0;
+							addSubChainNode(currentSub,remainingSize);
+							return currentSub->subAddr;
+						}
+					}
+					currentSub=currentSub->next;
+				}
+			}
+			currentMain = currentMain->next;
+		}
+		//No free memory available, has to create another node
+		struct mainChainNode* addedNode=addMainChainNode(currentMain,size);
+		currentMain->next=addedNode;
+		addedNode->prev=currentMain;
+		struct subChainNode* currentSub=(struct subChainNode*)mmap(NULL, sizeof(struct subChainNode), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+		if (subChainNode == MAP_FAILED) {
+			perror("mmap");
+			exit(1);
+		}
+		currentSub->subAddr=currentMain->mainAddr;
+		currentSub->segmentSize=size;
+		currentSub->is_hole=0;
+		//addSubChainNode work to be done
+	}
+	else{ //When not even single main chain node is initialized
+		struct mainChainNode* addedNode=addMainChainNode(head,size);
+		head->mainChainNode=addedNode;
+	}
 }
-
 
 /*
 this function print the stats of the MeMS system like
@@ -128,28 +176,6 @@ Parameter: Nothing
 Returns: Nothing but should print the necessary information on STDOUT
 */
 void mems_print_stats() {
-    int total_pages_utilized = 0;
-    size_t unused_memory = 0;
-
-    // Iterate through the main chain and sub-chains
-    struct MainChainNode* currentMain = mainChainHead;
-    while (currentMain) {
-        printf("Main Chain Node - MeMS Virtual Address: %p\n", currentMain->main_addr);
-
-        // Sub-chain statistics
-        struct SubChainNode* currentSub = currentMain->sub_chain;
-        while (currentSub) {
-            printf("\tSub Chain Node - MeMS Virtual Address: %p, Size: %zu, Type: %s\n", currentSub->sub_addr, currentSub->size, currentSub->is_hole ? "HOLE" : "PROCESS");
-            if (currentSub->is_hole) unused_memory += currentSub->size;            
-            currentSub = currentSub->next;
-        }
-		printf("/n");
-        total_pages_utilized++;
-        currentMain = currentMain->next;
-    }
-
-    printf("Total Pages Utilized: %d\n", total_pages_utilized);
-    printf("Unused Memory: %zu bytes\n", unused_memory);
 }
 
 
@@ -159,25 +185,7 @@ Parameter: MeMS Virtual address (that is created by MeMS)
 Returns: MeMS physical address mapped to the passed ptr (MeMS virtual address).
 */
 void* mems_get(void* v_ptr) {
-    struct MainChainNode* currentMain = mainChainHead;
-
-    // Iterate through the main chain
-    while (currentMain) {
-        struct SubChainNode* currentSub = currentMain->sub_chain;
-        while (currentSub) {
-            if (currentSub->sub_addr == v_ptr) {
-                // Return the MeMS physical address
-                return currentSub->sub_addr;
-            }
-            currentSub = currentSub->next;
-        }
-        currentMain = currentMain->next;
-    }
-    // If not found, return NULL or an appropriate error value
-    return NULL;
 }
-
-
 
 /*
 this function free up the memory pointed by our virtual_address and add it to the free list
@@ -185,5 +193,4 @@ Parameter: MeMS Virtual address (that is created by MeMS)
 Returns: nothing
 */
 void mems_free(void* v_ptr) {
-    
 }
